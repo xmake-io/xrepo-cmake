739195fc36e12e6eb632e55c11b531959e0ddb19
Chen Yufei <cyfdecyf@gmail.com>
2022-02-28 07:43:34 +0800

    workflow: more test unix-like systems.

---
+++ ci/test-unix.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -x
+set -e
+
+# First build.
+output=cmake.log.0
+cmake -DXREPO_PACKAGE_VERBOSE=ON example | tee $output
+grep -E "pcre2_INCLUDE_DIR" $output
+grep -E "pcre2_LINK_DIR" $output
+grep -E "pcre2_LINK_LIBRARIES" $output
+grep -E "pcre2_DEFINITIONS" $output
+grep -E "gflags_DIR" $output
+grep -E "glog_DIR" $output
+grep -E "zlib_INCLUDE_DIR" $output
+make
+
+# Check for cached variables.
+output=make.log.1
+touch example/CMakeLists.txt
+make | tee $output
+
+match_cached_output="already installed, using cached variables"
+
+grep -E "pcre2 $match_cached_output" $output
+grep -E "gflags 2.2.2 $match_cached_output" $output
+grep -E "glog $match_cached_output" $output
+grep -E "myzlib $match_cached_output" $output
+
+# Check for update of config lua script.
+output=make.log.2
+touch example/packages/glog.lua
+make | tee $output
+grep -E "pcre2 $match_cached_output" $output
+grep -E "gflags 2.2.2 $match_cached_output" $output
+grep -E "example/packages/glog.lua" $output
+grep -E "myzlib $match_cached_output" $output
+
