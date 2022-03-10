2d120fe12273e7a552221763f15763a5ddc36fb4
Chen Yufei <cyfdecyf@gmail.com>
2022-03-10 17:05:49 +0800

    Improve example code, fix typo.

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -2,17 +2,20 @@
 #include <glog/logging.h>
 #include <pcre2.h>
 
-using GFLAGS_NAMESPACE::SetUsageMessage;
-
 int main(int argc, char** argv)
 {
+    google::InitGoogleLogging(argv[0]);
+    gflags::SetUsageMessage("xrepo-cmake example app.");
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
+
+    FLAGS_logtostderr = 1;
+
+    LOG(INFO) << "hello xrepo";
+
     pcre2_general_context* gctx = pcre2_general_context_create(NULL, NULL, NULL);
     if (gctx) {
         pcre2_general_context_free(gctx);
     }
-    ::google::InitGoogleLogging(argv[0]);
-    FLAGS_logtostderr = 1;
-    SetUsageMessage("Usage message");
-    LOG(INFO) << "hello xrepo";
+
     return 0;
 }
